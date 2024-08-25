7d42a29e7c7ee184ffb2f12a8473dc3cfe0e2a79
Benjamin_solis <basolis@miuandes.cl>
2024-08-25 13:29:08 -0400

    agrege los atributos a la clase de ataque

---
+++ Ataque.py
@@ -0,0 +1,9 @@
+class ataque:
+    def __init__(self, nombre, tipo_ataque, objetivo, daño, precision, recarga):
+
+        self.nombre= nombre 
+        self.tipo_ataque= tipo_ataque
+        self.objetivo= objetivo
+        self.daño= daño
+        self.precision= precision
+        self.recarga= recarga
