fb96f3a0d160bf9310b24ed2be6020508ff96e8a
Benjamin_solis <basolis@miuandes.cl>
2024-08-25 17:30:51 -0400

    actualizacion de clases

---
+++ Liga.py
@@ -0,0 +1,3 @@
+class liga:
+    def __init__(self, robots):
+        self.robots = robots
---
+++ robots.py
@@ -0,0 +1,6 @@
+class Robots:
+    def __init__(self, name, energy, attacks):
+        self.name = name
+        self.energy = energy
+        self.attack = attacks
+        
