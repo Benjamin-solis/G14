83c3e1c2a919ae153dd2810aa3b7fe990bf6ab8b
Benjamin-solis <basolis@miuandes.cl>
2024-08-25 17:27:42 -0400

    Delete Ataque.py

--- Ataque.py
+++
@@ -1,9 +0,0 @@
-class ataque:
-    def __init__(self, name, type_attack, objetive, damage, precision, recharge):
-
-        self.name= name 
-        self.type_attack= type_attack
-        self.objetive= objetive
-        self.damage= damage
-        self.precision= precision
-        self.recharge= recharge
