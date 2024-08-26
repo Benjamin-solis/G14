9c79435a57c342b74bbe062b2688e2845adb4ee3
AndresBerriosL <aiberrios@miuandes.cl>
2024-08-26 15:56:01 -0400

    actualización: liga.py pinta que esta terminada

--- Liga.py
+++ Liga.py
@@ -19,11 +19,29 @@ class liga:
                                     x['precision'],
                                     x['recharge'])
                     robot.add_attack(ataque)
-                print(robot.get_name())
+                self.robots.append(robot)
+
+    def start(self):
+        for i in range(len(self.robots)):
+            robot_1 = self.robots[i]
+            for j in range(i+1, len(self.robots)):
+                robot_2 = self.robots[j]
+                self.pelea(robot_1, robot_2)
+
+    def pelea(self, r1, r2):
+        print(f"Empieza la pelea entre {r1.get_name()} y {r2.get_name()}")
+        actualm = r1
+        while r1.get_energy() > 0 and r2.get_energy() > 0:
+            mov = actualm.random_move()
+
+
+
                 
                     
 a = liga()
 a.load_robots('c:/Users/altad2/Desktop/paradigmas/Tarea1_Paradigmas/robots01.json')
+a.start()
+
                
                 
 
