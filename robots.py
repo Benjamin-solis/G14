424552a75cc1bfb38b6e2a8a6a1fa232775e86bf
AndresBerriosL <aiberrios@miuandes.cl>
2024-08-26 11:00:27 -0400

    Robots actualizado

--- robots.py
+++ robots.py
@@ -1,6 +1,23 @@
+import random
+
 class Robots:
-    def __init__(self, name, energy, attacks):
+    def __init__(self, name, energy):
         self.name = name
         self.energy = energy
-        self.attack = attacks
+        self.attacks = []
         
+    def get_name(self):
+        return self.name
+    
+    def get_energy(self):
+        return self.energy
+    
+    def add_attack(self, attack):
+        self.attacks.append(attack)
+    
+    def random_move(self):
+        return random.choice(self.attacks)
+    
+robot = Robots("timmy", 100)
+print(robot.get_name())
+print(robot.get_energy())
