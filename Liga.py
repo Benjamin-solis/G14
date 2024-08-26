03ea083415a5f2c4f54dcad64fa0fb66af738412
AndresBerriosL <aiberrios@miuandes.cl>
2024-08-26 11:16:46 -0400

    Pequeño avance, se debe de continuar

--- Liga.py
+++ Liga.py
@@ -1,3 +1,11 @@
 class liga:
-    def __init__(self, robots):
-        self.robots = robots
+    def __init__(self):
+        self.robots = []
+
+    def load_robots(self, filename):
+        with open(filename, 'r') as t:
+            data = json.file(t)
+            r_attacks = {}
+            for ra in data['robots']:
+                #r_attacks[ra['name']] =
+                # CONTINUAR 
