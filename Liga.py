77de978167af50d3a12fe9b164466c7513389954
Your Nameaarriagada@miaundes.c <aarriagada2@miaundes.cl>
2024-08-26 18:15:41 -0400

    Registrar quien gana/pierde/empata

--- Liga.py
+++ Liga.py
@@ -1,10 +1,12 @@
 import robots
 import attack
 import json
+import reportes 
 
 class liga:
     def __init__(self):
         self.robots = []
+        self.record = {}
 
     def load_robots(self, filename):
         with open(filename, 'r') as t:
@@ -20,6 +22,7 @@ class liga:
                                     x['recharge'])
                     robot.add_attack(ataque)
                 self.robots.append(robot)
+                self.record[ra['name']] = {'Won' : 0, 'Lost' : 0, 'Draw': 0}
 
     def start(self):
         for i in range(len(self.robots)):
@@ -45,10 +48,18 @@ class liga:
         
         if r1.get__energy() == 0 and r2.get_energy() == 0:
             print(f"Hubo un empate entre {r1.get_name()} y {r2.get_name()}")
-            self.draw_reports[r1.get_name()]['Empates'] += 1
-            self.draw_reports[r2.get_name()]['Empates'] += 1
+            self.record[r1.get_name()]['Draw'] += 1
+            self.record[r2.get_name()]['Draw'] += 1
+        elif r1.get__energy() > 0 and r2.get_energy() == 0:
+            print(f'{r1.get_name()} gana la batalla')
+            self.record[r1.get_name()]['Won'] += 1
+            self.record[r1.get_name()]['Lost'] += 1
+        else: 
+            print(f'{r2.get_name()} gana la batalla')
+            self.record[r2.get_name()]['Won'] += 1
+            self.record[r2.get_name()]['Lost'] += 1
+    
         
-            
                 
                     
 a = liga()
